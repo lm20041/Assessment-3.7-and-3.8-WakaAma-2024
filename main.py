5adfea681cd0e1b5201b5ad8a8a70a65d2cceb51
lm20041 <132313536+lm20041@users.noreply.github.com>
2024-05-01 04:38:53 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+from ldrawfile import LDrawFile
+
+# Open the LiF file
+lif_file_path = "path/to/your/file.lif"
+lif_file = LDrawFile(lif_file_path)
+
+# Access data from the LiF file
+print(lif_file.header)
+for step in lif_file.steps:
+    print(step)
