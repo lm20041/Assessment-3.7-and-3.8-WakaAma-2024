005b0f63bf60e71f824bc23d6d52718cf18bc8a5
lm20041 <132313536+lm20041@users.noreply.github.com>
2024-05-03 01:11:18 +0000

    open file.lif with loop (test2) success
    with a correct lif_file_path i was able to record the contacts of the of a file page onto a list line by line and made it appear on the screen. now i will attempt to display all the data from each file

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-lif_file_path ="waka_ama_db/3.7Bresourcefiles/WakaNats2017/001-Heat 1-01.lif"
+lif_file_path ="waka_ama_db/3.7B resource files/WakaNats2017/001-Heat 1-01.lif"
 keep_lines = []
 with open(lif_file_path, 'r') as f:
   for line in f:
