0e4c29778bbc62839ad079f82d7d794ad1d3b3ae
lm20041 <132313536+lm20041@users.noreply.github.com>
2024-05-13 00:16:49 +0000

    window link-program PhotoImage (test1)
    -i try to immediately input the PhotoImage code from my previous Rep let designs without making a smaller separate component to test.
    -Adjust the You would require to input the PhotoImage, subsampling to factors it's size and have an lifepath to that image.

--- main.py
+++ main.py
@@ -12,13 +12,19 @@ class program:
     self.parent_frame = Frame(self.master, bg="lightgrey", borderwidth=2, relief="ridge")
     self.parent_frame.grid(padx=20, pady=20)
     # ***** row0(text) *****
-    self.heading_label = Label(self.parent_frame, text="Window-Test1", font=button_font_12, fg=txt_fg, bg=background)
+    self.heading_label = Label(self.parent_frame, text="Welcome to Waka Ama", font=button_font_12, fg=txt_fg, bg=background)
     self.heading_label.grid(row=0, pady=5)
     # ***** row1(imag) *****
-
+    image_path = "ColorCard-image/color_card-back_card.png"
+    image_pit = PhotoImage(file=image_path)
+    self.image_label = Label(self.parent_frame, image=image_pit)
+    self.image_label.image = image_pit
+    self.image_label.grid(row=1, padx=10, pady=10)
+    self.create_widgets(num_cards, username)
     # ***** row2(text) *****
-    self.heading_label = Label(self.parent_frame, text="Window-Test1", font=button_font_12, fg=txt_fg, bg=background)
-    self.heading_label.grid(row=0, pady=5)
+    pro_text = "This program is created for the wakana culbs to read and write files recorded throughout their races."
+    self.text_label = Label(self.parent_frame, text=pro_text, font=button_font_12, fg=txt_fg, bg=background)
+    self.text_label.grid(row=2, pady=5)
     # ***** row3(button) *****
     self.button = Button(self.parent_frame, text="Start Program", font=button_font_12, fg=txt_fg, command=self.to_converter)
     self.button.grid(row=3, pady=5)
