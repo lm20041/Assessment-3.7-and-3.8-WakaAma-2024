f5c19a5a4d8cafbafee0725c49719bf6f74b3d46
lm20041 <132313536+lm20041@users.noreply.github.com>
2024-05-13 05:50:14 +0000

    window link-program frame setting (test2)
    finish screen
    the Var's setting are: background, bold_font_12, text_font_6, txt_fg

--- main.py
+++ main.py
@@ -3,17 +3,18 @@ from tkinter import PhotoImage
 
 class program:
   def __init__(self, master):
-    #Var's
+    #Var's background, bold_font_12, text_font_6, txt_fg
     background = "white"
-    button_font_12 = ("Arial", 12, "bold")
+    bold_font_12 = ("Arial", 12, "bold")
+    text_font_6 = ("Arial", 10)
     txt_fg = "black"
     self.master = master #self.Var
     # ***** Create frame *****
     # Create the parent frame
     self.parent_frame = Frame(self.master, bg="lightgrey", borderwidth=2, relief="ridge")
-    self.parent_frame.grid(padx=20, pady=20)
+    self.parent_frame.grid(padx=60, pady=10)
     # ***** row0(text) *****
-    self.heading_label = Label(self.parent_frame, text="Welcome to Waka Ama", font=button_font_12, fg=txt_fg, bg=background)
+    self.heading_label = Label(self.parent_frame, text="Welcome to Waka Ama", font=bold_font_12, fg=txt_fg)
     self.heading_label.grid(row=0, pady=5)
     # ***** row1(imag) *****
     image_path = "PhotoImage/culture_of_waka_ama-main-image.png"
@@ -24,10 +25,10 @@ class program:
     self.image_label.grid(row=1, padx=10, pady=10)
     # ***** row2(text) *****
     pro_text = "This program is created for the wakana culbs to read and write files recorded throughout their races."
-    self.text_label = Label(self.parent_frame, text=pro_text, wrap=200, font=button_font_12, fg=txt_fg, bg=background)
+    self.text_label = Label(self.parent_frame, text=pro_text, wrap=250, font=text_font_6, fg=txt_fg)
     self.text_label.grid(row=2, pady=5)
     # ***** row3(button) *****
-    self.button = Button(self.parent_frame, text="Start Program", font=button_font_12, fg=txt_fg, command=self.to_converter)
+    self.button = Button(self.parent_frame, text="Start Program", font=bold_font_12, fg=txt_fg, command=self.to_converter)
     self.button.grid(row=3, pady=5)
   def to_converter(self):
     print("test")
