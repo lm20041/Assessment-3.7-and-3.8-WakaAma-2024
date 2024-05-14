49a51dd364a6c2e97bea5cf547cd9d275492a2fc
lm20041 <132313536+lm20041@users.noreply.github.com>
2024-05-14 22:48:13 +0000

    component-create filepath(open pacific file-Test2) success
    changing file name to search term and have it search for files with new name file var "search_term" that end with .endswith(".lif") that can be found in the folder

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import os
 folder  = "waka_ama_db/3.7B resource files/WakaNats2017/"
-file = "Final"
+search_term = "Final"
 points = 0
 
 # List all .lif files in the directory
-lif_files = [file for file in os.listdir(folder) if file.endswith(file)]
-
+lif_files = [file for file in os.listdir(folder) if file.endswith(".lif") and search_term in file]
 # Print the full path of each .lif file
 for lif_file in lif_files:
     full_path = os.path.join(folder, lif_file)
